a0dadd061bc6d3a1017775b5c684fe27f9af3527
Alan Bradley <alan@gangleri.net>
2014-03-18 22:11:32 +0000

    changes from testing

--- install.js
+++ install.js
@@ -3,20 +3,22 @@
 var fs = require('fs'),
     path = require('path');
 
+console.log('Installing git pre-commit hook');
+
 var gitPath = path.resolve('./', '../..');
 
 while(!fs.existsSync(gitPath + '/.git')) {
-  if(gitPath !== '/') {
+  if(gitPath === '/') {
     console.log('No git repo found.');
     return 1;
   }
   gitPath = path.resolve(gitPath, '..');
 }
 
-var dest = path.resolve(gitPath, 'hook', 'pre-commit');
+var dest = path.resolve(gitPath, '.git', 'hooks', 'pre-commit');
 
 if(!fs.existsSync(dest)) {
-  fs.createReadStream(__dirname + '/pre-commit.js').pipe(fs.createWriteStream(dest, {mode: 755}));
+  fs.createReadStream(__dirname + '/pre-commit.js').pipe(fs.createWriteStream(dest, {mode: '0755'}));
 } else {
   console.log('pre-commit already exists!');
 }
